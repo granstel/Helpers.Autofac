8df39bff55ad2cfc4d962c214ecbbacd3b763eb4
Stepan Grankin <sgrankin@inbox.ru>
2019-02-20 09:10:21 +0300

    try to add files

---
+++ src/GranSteL.Helpers.Autofac/ISafeInvoker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GranSteL.Helpers.Autofac
+{
+    public interface ISafeInvoker<out T>
+    {
+        TOut Invoke<TOut>(Func<T, TOut> action);
+
+        Task<TOut> InvokeAsync<TOut>(Func<T, Task<TOut>> action);
+
+        void Invoke(Action<T> action);
+
+        Task InvokeAsync(Func<T, Task> action);
+    }
+}
---
+++ src/GranSteL.Helpers.Autofac/SafeInvoker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac.Features.OwnedInstances;
+
+namespace GranSteL.Helpers.Autofac
+{
+    public class SafeInvoker<T> : ISafeInvoker<T>
+    {
+        private readonly Func<Owned<T>> _buildDependency;
+
+        public SafeInvoker(Func<Owned<T>> buildDependency)
+        {
+            _buildDependency = buildDependency;
+        }
+
+        public TOut Invoke<TOut>(Func<T, TOut> action)
+        {
+            using (var dependency = _buildDependency())
+            {
+                return action(dependency.Value);
+            }
+        }
+
+        public async Task<TOut> InvokeAsync<TOut>(Func<T, Task<TOut>> action)
+        {
+            using (var dependency = _buildDependency())
+            {
+                return await action(dependency.Value);
+            }
+        }
+
+        public void Invoke(Action<T> action)
+        {
+            using (var dependency = _buildDependency())
+            {
+                action(dependency.Value);
+            }
+        }
+
+        public async Task InvokeAsync(Func<T, Task> action)
+        {
+            using (var dependency = _buildDependency())
+            {
+                await action(dependency.Value);
+            }
+        }
+    }
+}
